b231344efac571f8cc6f8d85086409e9b46d5e6d
BamanBoi <samsharma3244@gmail.com>
2023-07-26 16:02:17 +0530

    commit

---
+++ chatapp-server/models/user.js
@@ -0,0 +1,92 @@
+const mongoose= require("mongoose");
+const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
+const userSchema = new mongoose.Schema({
+   firstName:{
+    type: String,
+    required:[true,"First Name is required"],
+   },
+   lastName : {
+    type: String,
+    required: [true,"Last Name is required"],
+   },
+   avatar: {
+    type: String,
+   },
+   email: {
+    type: String,
+    required: [true,"Email is required"],
+    validate: {
+        validator: function name(email){
+            return String(email).toLowerCase().match(/^[A-Z0-9_'%=+!`#~$*?^{}&|-]+([\.][A-Z0-9_'%=+!`#~$*?^{}&|-]+)*@[A-Z0-9-]+(\.[A-Z0-9-]+)+$/i);
+        },
+        message: (props) => `Email (${props.value}) is invalid`,
+    },
+   },
+   password : {
+    type: String,
+   },
+   passwordConfirm: {
+     type: String,
+   },
+   passwordChangedAt: {
+    type: Date,
+   },
+   passwordResetToken: {
+    type: String,
+   },
+   passwordResetExpires : {
+    type: Date,
+   },
+   updatedAt: {
+    type: Date,
+   },
+   verified:{
+    type: Boolean,
+    default: false,
+   },
+   otp : {
+    type: Number,
+   },
+   otp_expiry_time : {
+    type: Date,
+   },
+});
+
+userSchema.pre("save",async function(next){
+    //only run if otp is actually modified
+   if(this.isModified("otp")) return next();
+    //hashing with cost of 12
+    this.otp =  (this.otp);
+
+    next();
+});
+
+userSchema.methods.correctPassword = async function (
+    candidatePassword,
+    userPassword,
+){
+    return await bcrypt.compare(candidatePassword,userPassword);
+}
+
+userSchema.methods.correctOTP = async function (
+    candidateOTP,
+    userOTP,
+){
+    return await bcrypt.compare(candidateOTP,userOTP);
+}
+
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString("hex");
+    this.passwordResetToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+
+    this.passwordResetExpires = Date.now()+ 10*60*1000;
+    return resetToken;
+}
+
+userSchema.methods.changedPasswordAfter = function(timestamp){
+   return timestamp < this.passwordChangedAt;
+}
+
+const User = new mongoose.model("User", userSchema);
+module.exports = User;
